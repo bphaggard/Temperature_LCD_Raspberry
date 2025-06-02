3bdfdc347739fd8feec3e8bbf9bb06225548ff79
Patrik Mlcoch <patrik.mlcoch@gmail.com>
2025-06-02 17:19:00 +0200

    DHT-11 and LCD1602 with Raspberry Pi 5

---
+++ temperature_main.py
@@ -0,0 +1,24 @@
+from temperature import DHT11
+from LCD import LCD
+import time
+
+dht11 = DHT11(17)
+LCD1602 = LCD(i2c_addr=0x27)
+try:
+    while True:
+        humidity, temperature = dht11.read_data()
+        #timestamp = time.time()
+        #time_object = datetime.datetime.fromtimestamp(timestamp).strftime("%H:%M:%S")
+        #print(f"{time_object}  temperature:{temperature}°C  humidity: {humidity}%")
+        #with open("temperature.txt", mode="a") as file:
+        #    timestamp = time.time()
+        #    time_object = datetime.datetime.fromtimestamp(timestamp).strftime("%H:%M:%S")
+        #    file.write(f"\n{time_object}  temperature:{temperature}°C  humidity: {humidity}%")
+        LCD1602.message(f"Teplota: {temperature}˚C", 1)
+        LCD1602.message(f"Vlhkost: {humidity}%", 2)
+        time.sleep(2)
+except KeyboardInterrupt:
+    LCD1602.clear()
+    LCD1602.message("Measurement stopped", 1)
+    time.sleep(4)
+    LCD1602.clear()
